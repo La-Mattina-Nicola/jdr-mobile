dc4d952acd652d6ceb261de8a583c692e5e1f355
La-Mattina-Nicola <nicola.lamattina@outlook.com>
2024-12-03 00:45:52 +0100

    update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:jdr/auth/auth_gate.dart';
+import 'package:jdr/services/auth/auth_gate.dart';
 import 'package:jdr/firebase_options.dart';
 
 void main() async {
